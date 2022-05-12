a66843f799c3870ea81dba859e1aac9d46beb452
Kristian Toustrup Langborg-Hansen <ktlh@smartlearning.dk>
2022-05-12 20:36:18 +0200

    Add project files.

---
+++ FindObjektIArray/Bil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindObjektIArray
+{
+    public class Bil
+    {
+        public string Farve { get; set; }
+        public string Mærke { get; set; }
+
+        public string Nummerplade { get; set; }
+
+    }
+}
---
+++ FindObjektIArray/Program.cs
@@ -0,0 +1,38 @@
+﻿// Lav nogle biler
+using FindObjektIArray;
+
+Bil[] bilpark = new Bil[4];
+
+bilpark[0] = new();
+bilpark[0].Farve = "Blå";
+bilpark[0].Mærke = "Ford";
+bilpark[0].Nummerplade = "DF30606";
+
+bilpark[1] = new();
+bilpark[1].Farve = "Blå";
+bilpark[1].Mærke = "Mazda";
+bilpark[1].Nummerplade = "AB52643";
+
+bilpark[2] = new();
+bilpark[2].Farve = "Blå";
+bilpark[2].Mærke = "Ford";
+bilpark[2].Nummerplade = "BC63356";
+
+bilpark[3] = new();
+bilpark[3].Farve = "Sort";
+bilpark[3].Mærke = "Ford";
+bilpark[3].Nummerplade = "VF53221";
+
+Console.WriteLine("Hvilken farve biler skal vi finde (prøv med Blå)");
+string farve = Console.ReadLine();
+
+Console.WriteLine("Hvilket bilmærke leder vi efter (prøv med Ford)");
+string mærke = Console.ReadLine();
+
+foreach (var bil in bilpark)
+{
+    if (bil.Farve == farve && bil.Mærke == mærke)
+    {
+        Console.WriteLine($"Fundet: {bil.Nummerplade}");
+    }
+}
